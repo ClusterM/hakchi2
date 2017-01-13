a1f5f8f13e95b735f70ecd3c8fe29505db2a73b6
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2017-01-13 14:43:05 +0300

    forget to add UnsupportedFourScreenException.cs, sorry

---
+++ UnsupportedFourScreenException.cs
@@ -0,0 +1,17 @@
+﻿using com.clusterrr.Famicom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.clusterrr.hakchi_gui
+{
+    public class UnsupportedFourScreenException : Exception
+    {
+        public readonly NesFile ROM;
+        public UnsupportedFourScreenException(NesFile nesFile)
+        {
+            ROM = nesFile;
+        }
+    }
+}
