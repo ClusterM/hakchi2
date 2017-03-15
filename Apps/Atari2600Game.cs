c80447c3eaa0b1bab4aff7b6500293f782678ec7
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2017-03-15 04:10:25 +0300

    Forgot some files

---
+++ Apps/Atari2600Game.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable 0108
+using com.clusterrr.hakchi_gui.Properties;
+using System.Drawing;
+
+namespace com.clusterrr.hakchi_gui
+{
+    public class Atari2600Game : NesMiniApplication
+    {
+        public override string GoogleSuffix
+        {
+            get
+            {
+                return "atari 2600";
+            }
+        }
+
+        public Atari2600Game(string path, bool ignoreEmptyConfig = false)
+            : base(path, ignoreEmptyConfig)
+        {
+        }
+    }
+}
+
