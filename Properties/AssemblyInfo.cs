4c9e52e3fa7626858da09833bf6c98e65c3c7202
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2018-07-26 00:37:16 +0300

    2.31

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.30.18")]
-[assembly: AssemblyFileVersion("2.0.30.18")]
+[assembly: AssemblyVersion("2.0.31.10")]
+[assembly: AssemblyFileVersion("2.0.31.10")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
