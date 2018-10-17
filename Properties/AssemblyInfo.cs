8f6922e0d056f462ca64d2fbc8a2788b69b8924d
Daniel <DanTheMan827@users.noreply.github.com>
2018-10-17 15:08:54 -0500

    Bump version to 3.5.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.1.12")]
-[assembly: AssemblyFileVersion("3.4.1.12")]
+[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
