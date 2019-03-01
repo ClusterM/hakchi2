d4cd8bfe3d139d43fd04c7f3d253125b08e5b3e0
Daniel <790119+DanTheMan827@users.noreply.github.com>
2019-03-01 17:11:40 -0600

    Typo: Estimate tile left > Estimate time left #202

--- Tasks/TaskerTransferForm.Designer.cs
+++ Tasks/TaskerTransferForm.Designer.cs
@@ -103,7 +103,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Estimated tile left:";
+            this.label1.Text = "Estimated time left:";
             // 
             // label2
             // 
