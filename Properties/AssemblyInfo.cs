df96d8d905df818476776c7657564f7bb1539189
DanTheMan827 <790119+DanTheMan827@users.noreply.github.com>
2022-03-15 15:48:09 -0500

    Version bump to 3.9.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -66,6 +66,6 @@ Gert Driesen for SSH.NET.")]
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.2.0")]
-[assembly: AssemblyFileVersion("3.9.2.0")]
+[assembly: AssemblyVersion("3.9.3.0")]
+[assembly: AssemblyFileVersion("3.9.3.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
